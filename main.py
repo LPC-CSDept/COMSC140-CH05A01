2560127a40e6debd3ef2f6812f894e8b51327bc5
Kayw Lee <kwooler@gmail.com>
2023-09-30 13:12:45 -0700

    Update

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ def sumoProduct(l1, l2):
     ##################################################
 
 
-numbers1 = [5, 3, 1, 1, 2]
-numbers2 = [1, 2, 2, 9, 5]
-result = sumoProduct(numbers1, numbers2)
-print('Your result : ', result)
+def main():
+    numbers1 = [5, 3, 1, 1, 2]
+    numbers2 = [1, 2, 2, 9, 5]
+    result = sumoProduct(numbers1, numbers2)
+    print('Your result : ', result)
 
 
-numbers1 = [random.randint(1, 10) for i in range(5)]
-numbers2 = [random.randint(1, 10) for i in range(5)]
-print('numbers 1 ', numbers1)
-print('numbers 2 ', numbers2)
-result = sumoProduct(numbers1, numbers2)
-print('Your result : ', result)
+    numbers1 = [random.randint(1, 10) for i in range(5)]
+    numbers2 = [random.randint(1, 10) for i in range(5)]
+    print('numbers 1 ', numbers1)
+    print('numbers 2 ', numbers2)
+    result = sumoProduct(numbers1, numbers2)
+    print('Your result : ', result)
